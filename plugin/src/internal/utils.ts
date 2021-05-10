0dab3953580f994f5d837c6039decc06da8d0f9f
Hyeseong Kim <hey@hyeseong.kim>
2021-05-11 04:59:23 +0900

    impl scheduler and test in an example project

--- plugin/src/internal/utils.ts
+++ plugin/src/internal/utils.ts
@@ -1,4 +1,6 @@
-import path from 'path';
+import fs from 'fs';
+import { dirname } from 'path';
+import { promisify } from 'util';
 import type { Store } from 'redux';
 import type {
   IGatsbyState as GatsbyState,
@@ -7,19 +9,41 @@ import type {
 } from 'gatsby/dist/redux/types';
 import type { OverrideProps } from '@cometjs/core';
 
+type Brand<T extends string> = { __BRAND__: T };
+
+const _mkdir = promisify(fs.mkdir);
+const _readFile = promisify(fs.readFile);
+const _writeFile = promisify(fs.writeFile);
+
+export const readFile = async (path: string): Promise<string> => {
+  return _readFile(path, { encoding: 'utf-8' });
+};
+
+export const writeFile = async (path: string, data: string | Buffer): Promise<void> => {
+  await _mkdir(dirname(path), { recursive: true });
+  await _writeFile(path, data, { encoding: 'utf-8' });
+};
+
+export const delay = (ms: number): Promise<void> => new Promise<void>(res => setTimeout(res, ms));
+
+export const formatLanguage = (lang: 'typescript' | 'flow'): 'TypeScript' | 'Flow' => (
+  (lang === 'typescript') ? 'TypeScript' : 'Flow'
+);
+
 export type GatsbyStore = Store<GatsbyState, GatsbyAction>;
 
 export type IDefinitionMeta = OverrideProps<_IDefinitionMeta, {
   // Trust me, this is more accurate.
   printedAst: string | null,
 }>;
+
 export type FragmentDefinition = OverrideProps<IDefinitionMeta, {
-  __BRAND__: 'FragmentDefinition',
   isFragment: true,
-}>;
+}> & Brand<'FragmentDefinition'>;
+
 export type QueryDefinition = OverrideProps<IDefinitionMeta, {
-  __BRAND__: 'QueryDefinition', isFragment: false,
-}>;
+  isFragment: false,
+}> & Brand<'QueryDefinition'>;
 
 export function isFragmentDefinition(def: IDefinitionMeta): def is FragmentDefinition {
   return def.isFragment;
@@ -34,7 +58,7 @@ export function isQueryDefinition(def: IDefinitionMeta): def is QueryDefinition 
  */
 export function guessIfUnnnamedQuery(def: QueryDefinition): boolean {
   const { name, filePath } = def;
-  const baseDir = path.dirname(filePath);
+  const baseDir = dirname(filePath);
   return name.startsWith(baseDir.split('/').join(''));
 }
 
